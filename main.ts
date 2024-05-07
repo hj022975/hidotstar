088370b3a65f8de31eee519a6636dd1f12ace265
hj022975 <67865647+hj022975@users.noreply.github.com>
2024-05-07 15:05:29 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -451,7 +451,7 @@ namespace hidotstar {
         strip._mode = HiDotStarMode.RGB_RGB;
         strip._matrixWidth = 0;
         strip.setBrightness(128);
-        pins.spiFrequency(1000000);
+        pins.spiFrequency(8000000);
         pins.spiFormat(8, 0);
         pins.spiPins(DigitalPin.P15, DigitalPin.P14, DigitalPin.P13);
         return strip;
